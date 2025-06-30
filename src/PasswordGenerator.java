10e7b6ae8fbee8bb940f97fcfbe5c588829d5033
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-06-30 15:38:47 +0200

    aggiunta parte finale con somma data nascita

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -8,14 +8,26 @@ public class PasswordGenerator {
         System.out.print("Inserisci il tuo nome: ");
         String nome = scan.next();
 
+        scan.nextLine();
+
         System.out.print("Inserisci il tuo cognome: ");
         String cognome = scan.nextLine();
 
         System.out.print("Inserisci il tuo colore preferito: ");
         String colore = scan.next();
 
-        System.out.print("Inserisci la tua data di nascita: ");
-        short dataNascita = scan.nextShort();
+        System.out.print("Inserisci il tuo giorno di nascita: ");
+        short giornoNascita = scan.nextShort();
+
+        System.out.print("Inserisci il tuo mese di nascita: ");
+        short meseNascita = scan.nextShort();
+
+        System.out.print("Inserisci il tuo anno di nascita: ");
+        short annoNascita = scan.nextShort();
+
+        int sommaNascita = giornoNascita + meseNascita + annoNascita;
+
+        System.out.println("La tua password è: " + nome + "-" + cognome + "-" + colore + "-" + sommaNascita);
 
     }
 }
